26b495f68c25d04654df70677d680037a3ef2dfb
D-Ngow <kyucmuathu3@gmail.com>
2024-10-26 16:55:02 +0700

    ProductDetail Update - Ngow

--- src/main/java/com/web/DAO/productImageDAO.java
+++ src/main/java/com/web/DAO/productImageDAO.java
@@ -1,9 +1,11 @@
 package com.web.DAO;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import com.web.Entity.ProductImage;
 
 public interface productImageDAO extends JpaRepository<ProductImage, Integer>{
-
+	@Query("SELECT p FROM ProductImage p WHERE p.product.productId =:id")
+    List<ProductImage> findByProductId(int id);
 }

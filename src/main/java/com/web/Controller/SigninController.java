948b68a360306317f299cfb72c939ca4e410015d
D-Ngow <kyucmuathu3@gmail.com>
2024-11-03 08:44:44 +0700

    update spring security - Ngow (Fail)

--- src/main/java/com/web/Controller/SigninController.java
+++ src/main/java/com/web/Controller/SigninController.java
@@ -1,17 +1,12 @@
 package com.web.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.web.Security.AccountDetail;
+import com.web.DAO.usersDAO;
 import com.web.Service.UserService;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -21,11 +16,12 @@ public class SigninController {
 	@Autowired
 	HttpServletRequest req;
 	
-	@Autowired
-	AuthenticationManager authManager;
 	
 	@Autowired
 	UserService usersv;
+	
+	@Autowired 
+	usersDAO userDAO;
 	
 	@GetMapping()	
 	private String signin() {
@@ -34,16 +30,18 @@ public class SigninController {
 	
 	@PostMapping("/submit")
 	public String postLogin() {
-		AccountDetail user = (AccountDetail) usersv.loadUserByUsername(req.getParameter("email"));
-//		System.out.println(user.getUsername());
-		if(user != null && user.getPassword().equals(req.getParameter("password"))){
-			Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-			SecurityContextHolder.getContext().setAuthentication(auth);
-			String savedUri = (String) req.getSession().getAttribute("uri");
-	        return "redirect:" + (savedUri != null ? savedUri : "/home");
-		}else {
-			return "signin";
-		}
+//		AccountDetail user = (AccountDetail) usersv.loadUserByUsername(req.getParameter("username"));
+//		if(user != null && user.getPassword().equals(req.getParameter("password"))){
+//			Authentication auth = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+//			SecurityContextHolder.getContext().setAuthentication(auth);
+//			String savedUri = (String) req.getSession().getAttribute("uri");
+//			System.out.println(savedUri);
+//	        return "redirect:/cart";
+//		}else {
+//			return "signin";
+//		}
+		
+		return "/home";
 		
 	}
 //	@PostMapping("/submit")

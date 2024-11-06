e75e3244d4cf1fa1035bf8ed8843985568709f31
Delvinn204 <45726655+Delvin204@users.noreply.github.com>
2024-11-06 16:30:05 +0700

    Update ProfileController.java

--- src/main/java/com/web/Controller/ProfileController.java
+++ src/main/java/com/web/Controller/ProfileController.java
@@ -1,10 +1,13 @@
 package com.web.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.web.Entity.Users;
@@ -17,20 +20,12 @@ public class ProfileController {
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("/{id}")
-	public Users GetProfile(@PathVariable int id) {
-
-		Users user = userService.findById(id);
-
-		return user;
+	@GetMapping()
+	public String GetProfile(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Users user = userService.findByEmail(auth.getName());
+		model.addAttribute("user", user);
+		return "profile";
 	}
-
-	@PutMapping()
-	public Users EditProfile (@RequestBody Users user) {
-
-		Users editUser = userService.EditProfile(user);
-		return editUser;
-	}
-
 
 }

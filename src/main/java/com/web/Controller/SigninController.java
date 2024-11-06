26d242bc650655e41e5889c4caaf600c7b0c2ffe
Delvinn204 <45726655+Delvin204@users.noreply.github.com>
2024-11-06 16:30:35 +0700

    Update SigninController.java

--- src/main/java/com/web/Controller/SigninController.java
+++ src/main/java/com/web/Controller/SigninController.java
@@ -1,25 +1,30 @@
 package com.web.Controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.web.DAO.usersDAO;
+import com.web.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/signin")
 public class SigninController {
-
-	@GetMapping()
+	
+	@Autowired
+	HttpServletRequest req;
+	
+	
+	@Autowired
+	UserService usersv;
+	
+	@Autowired 
+	usersDAO userDAO;
+	
+	@GetMapping()	
 	private String signin() {
 		return "signin";
 	}
-
-	@GetMapping("/success")
-	public ResponseEntity<String> successfully()
-	{
-
-		return ResponseEntity.ok("Login is successfully");
-	}
-
-
 }

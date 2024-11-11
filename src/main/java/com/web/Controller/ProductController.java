a018a260c777096012bac0348b3367798a299f72
D-Ngow <kyucmuathu3@gmail.com>
2024-11-11 16:16:00 +0700

    update filler product - Ngow - 11-11-24

--- src/main/java/com/web/Controller/ProductController.java
+++ src/main/java/com/web/Controller/ProductController.java
@@ -1,20 +1,16 @@
 package com.web.Controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import com.web.DAO.productsDAO;
 import com.web.Entity.Products;
-
 import jakarta.servlet.http.HttpServletRequest;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -22,20 +18,52 @@ public class ProductController {
 	productsDAO proDAO;
 	@Autowired
 	HttpServletRequest req;
-	
+
 	@GetMapping("product")
-	public String getProduct(Model model) {
-		List<Products> proList = proDAO.findAll();
-		model.addAttribute("proList", proList);
+	public String getProduct(Model model, @RequestParam(value = "brand", required = false) List<String> brands,
+			@RequestParam(value = "color", required = false) List<String> colors,
+			@RequestParam(value = "price", required = false) String price) {
+		if(price==null) {
+			List<Products> proList = proDAO.findByBrandAndColor(brands, colors);
+			model.addAttribute("proList", proList);
+		}else {
+			Sort sort = price.equals("0") ? Sort.by(Sort.Order.asc("price")) : Sort.by(Sort.Order.desc("price"));
+			List<Products> proList = proDAO.findByBrandInAndColorIn(brands, colors, sort);
+			model.addAttribute("proList", proList);
+		}
+		List<String> listbrand = proDAO.findBybrand();
+		model.addAttribute("listbrand", listbrand);
+		List<String> listcolor = proDAO.findBycolor();
+		model.addAttribute("listcolor", listcolor);
 		return "product";
 	}
-	
+
 	@PostMapping("/search")
 	public String search(Model model) {
-		List<Products> proSearch = proDAO.findByName("%"+req.getParameter("search")+"%");
+		List<Products> proSearch = proDAO.findByNameContaining("%" + req.getParameter("search") + "%");
 		model.addAttribute("proList", proSearch);
 		return "product";
 	}
-		
-	
+
+//	@PostMapping("/filter-products")
+//	public String filterProducts(@RequestParam(value = "brand", required = false) List<String> brands,
+//			@RequestParam(value = "color", required = false) List<String> colors, Model model) {
+//
+//		System.out.println("Selected Brands: " + brands);
+//		System.out.println("Selected Colors: " + colors);
+//
+//		if (brands == null) {
+//			brands = List.of(); 
+//		}
+//		if (colors == null) {
+//			colors = List.of(); 
+//		}
+//		List<Products> prolist = proDAO.findByBrandAndColor(brands, colors);
+//		for (Products p : prolist) {
+//			System.out.println(p.getProductName());
+//		}
+//		model.addAttribute("proList", prolist);
+//		return "product";
+//	}
+
 }

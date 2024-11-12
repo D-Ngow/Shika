3c1604a0a19d543be66daa2e8324bf9ef7a24f24
D-Ngow <kyucmuathu3@gmail.com>
2024-11-12 16:51:06 +0700

    update link product home - Ngow

--- src/main/java/com/web/Controller/ProductController.java
+++ src/main/java/com/web/Controller/ProductController.java
@@ -9,7 +9,6 @@ import com.web.DAO.productsDAO;
 import com.web.Entity.Products;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -56,6 +55,10 @@ public class ProductController {
 	public String search(Model model) {
 		List<Products> proSearch = proDAO.findByNameContaining("%" + req.getParameter("search") + "%");
 		model.addAttribute("proList", proSearch);
+		List<String> listbrand = proDAO.findBybrand();
+		model.addAttribute("listbrand", listbrand);
+		List<String> listcolor = proDAO.findBycolor();
+		model.addAttribute("listcolor", listcolor);
 		return "product";
 	}
 

d883d01d7b59e2c08b1579aade75dbc86bdd40dd
D-Ngow <kyucmuathu3@gmail.com>
2024-11-21 16:04:43 +0700

    update category - Ngow

--- src/main/java/com/web/RestController/AdminCategoryRestController.java
+++ src/main/java/com/web/RestController/AdminCategoryRestController.java
@@ -3,23 +3,23 @@ package com.web.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.web.DAO.categoriesDAO;
 import com.web.DAO.categorydetailsDAO;
 import com.web.DAO.productsDAO;
 import com.web.Entity.Categories;
 import com.web.Entity.CategoryDetails;
 import com.web.Entity.Products;
+import com.web.Service.UploadService;
 
+import java.io.File;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -31,7 +31,8 @@ public class AdminCategoryRestController {
 	categorydetailsDAO cdDAO;
 	@Autowired
 	productsDAO proDAO;
-	
+	@Autowired
+	UploadService uploadService;
 	@GetMapping("/findAll")
 	public List<Categories> findAll() {
 		List<Categories> cateList = cateDAO.findAll();
@@ -41,6 +42,11 @@ public class AdminCategoryRestController {
 	@DeleteMapping("/deletePro/{cateId}/{proId}")
 	public void deletePro(@PathVariable int cateId, @PathVariable int proId) {
 		cdDAO.deleteProductByCategoryIdAndProductId(cateId, proId);
+	}
+	
+	@DeleteMapping("/deleteCate/{cateId}")
+	public void deletecategory(@PathVariable int cateId) {
+		cateDAO.deleteById(cateId);
 	}
 	
 	@GetMapping("/findPro/{id}")
@@ -67,6 +73,26 @@ public class AdminCategoryRestController {
 			System.out.println(e);
 		}
 		return false;
+	}
+	
+	@PostMapping(value = "/addCate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void addcate(@RequestParam(value="name") String name,
+						@RequestParam(value ="image") MultipartFile image){
+			Categories cate = new Categories();
+			try {
+				cate.setName(name);
+				if (image != null && !image.isEmpty()) {
+		            File imagePath = uploadService.save(image, "/image/category/");
+		            cate.setImage(imagePath.getName());
+		        }
+		        else {
+		        	throw new NullPointerException("Can Find Image");
+		        }
+				cateDAO.save(cate);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			
 	}
 	
 }
